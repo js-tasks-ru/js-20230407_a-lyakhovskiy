365ce5ba7103566900dc4d86a318edc914eee50c
a.lyakhovskiy <a.lyakhovskiy@qiwi.com>
2023-04-17 15:07:45 +0300

    -

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,15 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+  const inObj = Object.entries(obj);
+  const objNew = obj;
 
+  for (const el of fields) {
+    for (const element of inObj) {
+      if (element[0] === el) {
+        delete objNew[el];
+      }
+    }
+  }
+  return objNew;
 };
