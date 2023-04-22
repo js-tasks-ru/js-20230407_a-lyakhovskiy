3848c3e633adac7f0cc769643a464b4bb97879e6
a.lyakhovskiy <a.lyakhovskiy@qiwi.com>
2023-04-22 21:21:07 +0300

    -

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,16 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  let arr = path.split('.');
+  return function (obj) {
+    let newObj = obj;
+    for (let i = 0; i < arr.length; i++) {
+      if (newObj[arr[i]] === undefined) {
+        return;
+      }
+      newObj = newObj[arr[i]];
+    }
+    return newObj;
+  };
 }
+
