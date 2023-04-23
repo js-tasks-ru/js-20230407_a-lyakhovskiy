6f746ef9a2e3a7c314dba36c46fce32d30d3baf2
a.lyakhovskiy <a.lyakhovskiy@qiwi.com>
2023-04-23 19:03:00 +0300

    -

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,14 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-
+  let newArray = [];
+  if (arr === undefined) {
+    return newArray;
+  }
+  let set = new Set();
+  for (const element of arr) {
+    set.add(element);
+  }
+  newArray = [...set];
+  return newArray;
 }
